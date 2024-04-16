b8f61633c57275dfd0e205c2d2de6aeeee0fe503
s-prechtl <stefan@tague.at>
2024-04-16 14:30:57 +0200

    feat: leetcode lelele

---
+++ lua/custom/plugins/leetcode.lua
@@ -0,0 +1,14 @@
+return {
+    "kawre/leetcode.nvim",
+    build = ":TSUpdate html",
+    dependencies = {
+        "nvim-telescope/telescope.nvim",
+        "nvim-lua/plenary.nvim", -- required by telescope
+        "MunifTanjim/nui.nvim",
+        -- optional
+        "rcarriga/nvim-notify",
+    },
+    opts = {
+        lang="go"
+    },
+}
