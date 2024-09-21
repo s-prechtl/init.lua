58fa99335ecaba4ad9cdc7fef0f1494d3cf7c60e
s-prechtl <stefan@tague.at>
2024-09-21 13:38:20 +0200

    feat: hide .. and fix maps

--- lua/custom/plugins/oil.lua
+++ lua/custom/plugins/oil.lua
@@ -8,8 +8,26 @@ return {
   config = function()
     require('oil').setup {
       use_default_keymaps = false,
+      keymaps = {
+          ["g?"] = "actions.show_help",
+          ["<CR>"] = "actions.select",
+          ["<C-p>"] = "actions.preview",
+          ["<C-c>"] = "actions.close",
+          ["<C-l>"] = "actions.refresh",
+          ["-"] = "actions.parent",
+          ["_"] = "actions.open_cwd",
+          ["`"] = "actions.cd",
+          ["~"] = { "actions.cd", opts = { scope = "tab" }, desc = ":tcd to the current oil directory" },
+          ["gs"] = "actions.change_sort",
+          ["gx"] = "actions.open_external",
+          ["g."] = "actions.toggle_hidden",
+          ["g\\"] = "actions.toggle_trash",
+      },
       view_options = {
         show_hidden = true,
+        is_always_hidden = function(name, bufnr)
+          return name == ".."
+        end
       },
     }
 
